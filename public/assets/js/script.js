b4a2ce651cc9ea6340d17c16a67fef5ee97419d4
Jeff Zenko <jeff.zenko@gmail.com>
2023-03-25 18:11:03 -0400

    every great journey begins with the first step

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,22 +1,25 @@
 
-const apiBtn = document.getElementById('api-test');
-apiBtn.addEventListener('click', apiCall);
+const searchBtn = document.getElementById('search');
+searchBtn.addEventListener('click', apiCall);
 const apiKey = 'ef20bc3f8fc3da07cc17e6cea84240e1';
 var city = '';
 var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 function apiCall() {
-
+    //trim search, make sure it's a valid search then continue to promise
+    //else error signal
 
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
     })
-    .then (function (data){
-        console.log(data);
+    .then (function (weatherData){
+        console.log(weatherData);
         console.log('function');
-        console.log(data.city);
+        console.log(weatherData);
     })
+
+
 }
 
 
