ec6b17e703528d848a51076a53a480b33a456f4c
Jeff Zenko <jeff.zenko@gmail.com>
2023-03-25 19:54:54 -0400

    js update and deployment

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,10 @@
 const searchForm = document.getElementById('search-form');
 const apiKey = 'ef20bc3f8fc3da07cc17e6cea84240e1';
+var units = 'imperial';
+//optional units = imperial, metric, standard
 //pass city to promise to get data - weatherData
 function getWeather(city) {
-    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
@@ -13,6 +15,7 @@ function getWeather(city) {
         console.log(weatherData);
     })
 }
+
 //search city functionality
 searchForm.addEventListener('submit', function(searchCity) {
     searchCity.preventDefault();
@@ -26,3 +29,6 @@ searchForm.addEventListener('submit', function(searchCity) {
         console.log('something unexpected so far?')
     }
 });
+
+//let's start out with 12PM for each day. json returns an array with length 40
+//each day in 3 hour increments, so 5 days
