cd5f07aa146309ddbec33a0618f851157cdabc23
Jeff Zenko <jeff.zenko@gmail.com>
2023-03-27 06:15:35 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,10 @@ window.addEventListener('load', function(){ //on load see if there's anything in
             addPast(city);
         });
     }
+    //if (cachedCity.length === 0) {
+    //    pastSearches.hidden = true;
+    //}
+
 });
 
 const searchForm = document.getElementById('search-form');
@@ -114,7 +118,8 @@ function addPast(location) {
     pastCityEle.appendChild(pastCityButton);
 
     if (pastCityButton) {
-        pastCityButton.classList.add('py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'.replace(/ /g, '-'));
+
+        pastCityButton.classList.add('btn-past');
     }
 
 
